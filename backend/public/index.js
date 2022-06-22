c501ff2355702571175e780c42d2ade01d324762
Jayme Coulton <coultonjayme@yahoo.com>
2022-06-22 17:44:09 -0500

    update cors allow

--- backend/public/index.js
+++ backend/public/index.js
@@ -5,16 +5,9 @@ var express = require("express");
 var cheerio = require("cheerio");
 var cors = require("cors");
 var app = express();
-// const corsOptions = {
-//   headers: {
-//     "Access-Control-Allow-Origin": '*',
-//     "Access-Control-Allow-Headers: 'content-type',
-//   },
-// };
-// app.use(cors(corsOptions));
 app.use(cors());
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000/"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin", "dinner-draft.vercel.app/");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
