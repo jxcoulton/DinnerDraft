b5b757cb0723db6e187bfd4983298b687f68db7c
Jayme Coulton <coultonjayme@yahoo.com>
2022-06-22 17:28:57 -0500

    updated cors allow request

--- backend/public/index.ts
+++ backend/public/index.ts
@@ -10,11 +10,11 @@ const app = express();
 app.use(cors());
 
 app.use(function (req: any, res: any, next: any) {
-  res.header("Access-Control-Allow-Origin", "https://dinner-draft.vercel.app/"); // update to match the domain you will make the request from
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
+  res.header("Access-Control-Allow-Origin", "https://dinner-draft.vercel.app/");
+  // res.header(
+  //   "Access-Control-Allow-Headers",
+  //   "Origin, X-Requested-With, Content-Type, Accept"
+  // );
   next();
 });
 
