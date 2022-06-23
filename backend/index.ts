4dc21abf3c0ea79031955ed8f8c29f910ae40942
Jayme Coulton <coultonjayme@yahoo.com>
2022-06-22 19:45:56 -0500

    update cors allow

--- backend/index.ts
+++ backend/index.ts
@@ -10,11 +10,9 @@ const app = express();
 app.use(cors());
 
 app.use(function (req: any, res: any, next: any) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
+  res.header("Access-Control-Allow-Origin", "dinner-draft.vercel.app/");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header("Access-Control-Allow-Methods", "POST");
   next();
 });
 
